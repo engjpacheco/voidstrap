21c1d0164b1d653e7115691a64e726e7179298e9
Javier Pacheco <jpacheco@cock.li>
2023-06-16 04:10:04 -0500

    some changes

--- config.sh
+++ config.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-partitions () {
-    cfdisk -z /dev/sda
-    mkfs.vfat /dev/sda1
-    mkfs.ext4 /dev/sda3
-    mkswap /dev/sda2
-}
+cfdisk -z /dev/sda
+mkfs.vfat /dev/sda1
+mkfs.ext4 /dev/sda3
+mkswap /dev/sda2
 
-mount_partitions () {
-    mount /dev/sda3 /mnt
-    mkdir -p /mnt/boot/efi
-    mount /dev/sda1 /mnt/boot/efi
-    swapon /dev/sda2
-}
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+swapon /dev/sda2
 
-system_instalation () {
-    export XBPS_ARCH=x86_64-musl && xbps-install -Suy -R http://mirrors.servercentral.com/voidlinux/current/musl -r /mnt \
+export XBPS_ARCH=x86_64-musl && xbps-install -Suy -R http://mirrors.servercentral.com/voidlinux/current/musl -r /mnt \
     xbps \
     base-minimal \
     vim \
@@ -45,7 +40,7 @@ system_instalation () {
     file \
     gcc \
     mesa-vaapi \
-    mksh \
+    zsh \
     mpv \
     fzf \
     openssh \
@@ -65,73 +60,53 @@ system_instalation () {
     linux-firmware-intel \
     iputils
 
-    for dir in sys dev proc; do $(mount --rbind /$dir /mnt/$dir && mount --make-rslave /mnt/$dir); done
-    cp /etc/resolv.conf /mnt/etc
-    cp /etc/xbps.d/* /mnt/etc/xbps.d/
-}
+for dir in sys dev proc; do $(mount --rbind /$dir /mnt/$dir && mount --make-rslave /mnt/$dir); done
+cp /etc/resolv.conf /mnt/etc
+cp /etc/xbps.d/* /mnt/etc/xbps.d/
+cp postinstall.sh /root/
 
-chroot_install () {
-    chroot /mnt /bin/sh <<EOF
-    ETHCARD="$(ip addr | grep ^2: | awk '{print $2}' | cut -d : -f 1)"
-    xbps-install -Sy grub-x86_64-efi
-    status=$?
-    if [ $status -eq 16 ]; then
-	xbps-install -uy xbps && xbps-install -uy grub-x86_64-efi
-    fi
+xchroot /mnt /bin/sh <<EOF
+xbps-install -Sy grub-x86_64-efi
+status=$?
+if [ $status -eq 16 ]; then
+    xbps-install -uy xbps && xbps-install -uy grub-x86_64-efi
+fi
 
-    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="void"
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="void"
 
-    echo void > /etc/hostname
+echo void > /etc/hostname
 
-    printf "
-    # /etc/rc.conf - system configuration for void-linux
+printf "
+# /etc/rc.conf - system configuration for void-linux
 
-    # Set the host name.
-    # HOSTNAME="void"
+# Set the host name.
+# HOSTNAME="void"
 
-    # Set RTC to UTC or localtime.
-    HARDWARECLOCK="UTC"
-    TIMEZONE=America/Matamoros
+# Set RTC to UTC or localtime.
+HARDWARECLOCK="UTC"
+TIMEZONE=America/Matamoros
 
-    # Keymap to load, see loadkeys(8).
-    KEYMAP=us\n" > /etc/rc.conf
+# Keymap to load, see loadkeys(8).
+KEYMAP=us\n" > /etc/rc.conf
 
-    echo "generating fstab file..."
-    printf "
-    /dev/sda1   /boot/efi   vfat    defaults,noatime,nodiratime        0   2
-    /dev/sda3   /           ext4    defaults,noatime,nodiratime        0   1
-    /dev/sda2   swap        swap    defaults                0   0
-    tmpfs       /tmp        tmpfs   defaults,nosuid,nodev,nodiratime   0   0
-    #tmpfs       /home/javier/.local/src/void-packages/masterdir/builddir    tmpfs   defaults,noatime,nodiratime,size=2G    0   0" > /etc/fstab
+echo "generating fstab file..."
+printf "
+/dev/sda1   /boot/efi   vfat    defaults,noatime,nodiratime        0   2
+/dev/sda3   /           ext4    defaults,noatime,nodiratime        0   1
+/dev/sda2   swap        swap    defaults                0   0
+tmpfs       /tmp        tmpfs   defaults,nosuid,nodev,nodiratime   0   0
+#tmpfs       /home/javier/.local/src/void-packages/masterdir/builddir    tmpfs   defaults,noatime,nodiratime,size=2G    0   0" > /etc/fstab
 
-    echo "Fstab file generated..."
+echo "Fstab file generated..."
 
-    xbps-reconfigure -fa
+xbps-reconfigure -fa
 
-    echo "Change root password..."
-    passwd
-    
-    chown root:root /
-    chmod 755 /	
-    
-    useradd -m -s /bin/mksh -U -G wheel,disk,lp,audio,video,optical,storage,scanner,network,plugdev,xbuilder javier
-    
-    echo "Change user password..."
-    passwd javier
+chown root:root /
+chmod 755 /	
 
-    echo "permit nopass root" > /etc/doas.conf
-    echo "permit nopass keepenv :wheel" >> /etc/doas.conf
 
-    rm /var/service && ln -sf /etc/runit/runsvdir/current /var/service
+echo "permit nopass root" > /etc/doas.conf
+echo "permit nopass keepenv :wheel" >> /etc/doas.conf
 
-    # Ethernet conection:
-    cp -R /etc/sv/dhcpcd-eth0 /etc/sv/dhcpcd-$ETHCARD
-    sed -i 's/eth0/$ETHCARD/' /etc/sv/dhcpcd-$ETHCARD/run
-    ln -s /etc/sv/dhcpcd-$ETHCARD /var/service/
-    EOF
-}
-
-partitions || echo "Something went wrong, please check you partitions..."
-mount_partitions || echo "Something went wrong mounting, please check you partitions..."
-system_instalation && echo "Done..." || echo "Check your internet conection or the ssl verification..."
-chroot_install || echo "There is an issue in the installation process..."
+rm /var/service && ln -sf /etc/runit/runsvdir/current /var/service
+EOF
